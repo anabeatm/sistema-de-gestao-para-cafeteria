fd58da04dd59b8666e9a32c93d68b990e9383685
Eliandro Gomes <eliandro.gomes42@gmail.com>
2026-04-02 14:02:50 -0300

    feat: add InventoryCategory and InventoryUnit enums

---
+++ projeto_cafeteria/lib/models/enums/inventory_enums.dart
@@ -0,0 +1,22 @@
+enum InventoryCategory {
+  base,
+  dairy,
+  fruit,
+  root,
+  herb,
+  sweetener,
+  tea,
+  spice,
+  sweet,
+  cleaning,
+  equipment,
+  other
+}
+
+enum InventoryUnit {
+  un,
+  g,
+  kg,
+  ml,
+  l
+}
